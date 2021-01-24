ad3765251f823cb7322951c010b72c1bcf33b608
Vichet <54062393+vichetmoeng@users.noreply.github.com>
2021-01-24 15:07:13 +0700

    update cashier route and view

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class CashierController extends Controller
 {
-    //
+    // page of cashier
+    public function index() {
+        return view('cashier.index');
+    }
 }
