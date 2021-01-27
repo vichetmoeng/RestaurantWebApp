889a0463111610aeffb0f60861cc1aee62a3efba
Vichet <54062393+vichetmoeng@users.noreply.github.com>
2021-01-28 05:14:26 +0700

    update show report with data of sale  saleDetails and report

--- resources/views/report/showReport.blade.php
+++ resources/views/report/showReport.blade.php
@@ -29,6 +29,52 @@
                         <p>The Total Amount of Sale from {{$dateStart}} to {{$dateEnd}} is ${{number_format($totalSale, 2)}}</p>
                         <p>Total Result: {{$sales->total()}}</p>
                     </div>
+                    <table class="table">
+                        <thead>
+                            <tr class="bg-primary text-light">
+                                <th scope="col">#</th>
+                                <th scope="col">Receipt ID</th>
+                                <th scope="col">Date Time</th>
+                                <th scope="col">Table</th>
+                                <th scope="col">Staff</th>
+                                <th scope="col">Total Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $countSale = ($sales->currentPage() - 1) * $sales->perPage() + 1;
+                            @endphp
+                            @foreach($sales as $sale)
+                                <tr class="bg-primary text-light">
+                                    <td>{{$countSale++}}</td>
+                                    <td>{{$sale->id}}</td>
+                                    <td>{{date("m/d/Y H:i:s", strtotime($sale->updated_at))}}</td>
+                                    <td>{{$sale->table_name}}</td>
+                                    <td>{{$sale->user_name}}</td>
+                                    <td>{{$sale->total_price}}</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>Menu ID</th>
+                                    <th>Menu</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    <th>Total Price</th>
+                                </tr>
+                                @foreach($sale->saleDetails as $saleDetail)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{$saleDetail->menu_id}}</td>
+                                        <td>{{$saleDetail->menu_name}}</td>
+                                        <td>{{$saleDetail->quantity}}</td>
+                                        <td>{{$saleDetail->menu_price}}</td>
+                                        <td>{{$saleDetail->menu_price * $saleDetail->quantity}}</td>
+                                    </tr>
+                                @endforeach
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{$sales->appends($_GET)->links()}}
                 @else
                     <div class="alert alert-danger" role="alert">
                         There is no Sale Report! Thanks!
