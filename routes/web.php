1fe1841be37d57b65812bd7fadf597b8c519c897
Vichet <54062393+vichetmoeng@users.noreply.github.com>
2021-01-28 06:05:33 +0700

    add middleware to route allow only loggedin user

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,31 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/management', function () {
-    return view('management.index');
+// middleware to allow access for login user only
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/management', function () {
+        return view('management.index');
+    });
+    Route::resource('management/category', 'Management\CategoryController');
+    Route::resource('management/menu', 'Management\MenuController');
+    Route::resource('management/table', 'Management\TableController');
+
+
+    Route::get('/cashier', 'Cashier\CashierController@index');
+    Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
+    Route::get('/cashier/gettable', 'Cashier\CashierController@getTable');
+    Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
+    Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+    Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+    Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+    Route::post('/cashier/savePayment', 'Cashier\CashierController@savePaymentInfo');
+    Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+
+    Route::get('/report', 'Report\ReportController@index');
+    Route::get('/report/show', 'Report\ReportController@show');
+    Route::get('/report/show/export', 'Report\ReportController@exportToExcel');
+
+
 });
-Route::resource('management/category', 'Management\CategoryController');
-Route::resource('management/menu', 'Management\MenuController');
-Route::resource('management/table', 'Management\TableController');
 
-
-Route::get('/cashier', 'Cashier\CashierController@index');
-Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
-Route::get('/cashier/gettable', 'Cashier\CashierController@getTable');
-Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
-Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
-Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
-Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
-Route::post('/cashier/savePayment', 'Cashier\CashierController@savePaymentInfo');
-Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
-
-Route::get('/report', 'Report\ReportController@index');
-Route::get('/report/show', 'Report\ReportController@show');
-Route::get('/report/show/export', 'Report\ReportController@exportToExcel');
