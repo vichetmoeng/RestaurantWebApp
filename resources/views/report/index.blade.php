cda5a638adc4ed48028db5a17913050d51d3330b
Vichet <54062393+vichetmoeng@users.noreply.github.com>
2021-01-28 04:00:24 +0700

    update report interface

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -1,1 +1,26 @@
-<h1>this is report page</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/home">Home</a> </li>
+                        <li class="breadcrumb-item active" aria-current="page">Report</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+@endsection
+
